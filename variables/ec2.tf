772cf2d9aafcbc478dd9f23c3ea3b0c77990e17a
Gowtham <gowthambabu8@gmail.com>
2026-02-20 23:25:10 +0530

    terraform changes

---
+++ variables/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "demo_instance"{
+    ami =  var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [ aws_security_group.allows_all_tls.id ]
+    tags = var.ec2_tags
+}
+
+resource "aws_security_group" "allows_all_tls"{
+    name = "allow_all-traffic"
+    description = "Allow inbound and outbound traffic"
+
+    tags = {
+      Name = "terraform"
+      Project = "roboshop"
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+}
---
+++ variables/variables.tf
@@ -0,0 +1,33 @@
+variable "ami_id" {
+    type = string
+    description = "RHEL 9"
+    default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+  type = string
+  description = "instance type"
+  default = "t3.micro"
+}
+
+variable "ec2_tags" {
+  type = map
+  description = "insance tags"
+  default = {
+    Name = "terraform-roboshop"
+    Project = "Roboshop"
+    Terraform  = true
+    Environment = "dev"
+  }
+}
+
+variable "sg_tags" {
+    type = map 
+    description = "security group tags"
+    default = {
+        Name = "allow-all-traffic"
+        Project = "Roboshop"
+        Terraform  = true
+        Environment = "dev"
+    }
+}
